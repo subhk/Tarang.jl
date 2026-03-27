3af760b0022e86a85b3773b386b1da48971d197c
Subhajit Kar <subhajitkar19@gmail.com>
2026-03-27 15:32:39 +0000

    Update test_aqua.jl

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -4,7 +4,8 @@ using Tarang
 @testset "Aqua.jl" begin
     Aqua.test_all(
         Tarang;
-        ambiguities=false,  # skip due to broad operator dispatch
-        piracies=false,     # skip due to Base method extensions
+        ambiguities=false,       # skip due to broad operator dispatch
+        piracies=false,          # skip due to Base method extensions
+        persistent_tasks=false,  # skip due to MPI.Init() in __init__
     )
 end
