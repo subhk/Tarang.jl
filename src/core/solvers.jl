f465cb7fa5b625f6dae8f6a871a72b07abf45206
Subhajit Kar <subhajitkar19@gmail.com>
2026-04-12 10:54:26 +0100

    Update solvers.jl

--- src/core/solvers.jl
+++ src/core/solvers.jl
@@ -4,11 +4,11 @@ Solver implementations for Tarang.jl
 Split into focused sub-files:
 - solver_types.jl: Solver definitions and constructors
 - solver_stepping.jl: Time stepping, BVP/EVP solve
-- solver_compiled_rhs.jl: RHS compilation and execution
+- lazy_rhs.jl: Type-specialized lazy RHS evaluation with broadcasting fusion
 - solver_utils.jl: Diagnostics and exports
 """
 
 include("solvers/solver_types.jl")
 include("solvers/solver_stepping.jl")
-include("solvers/solver_compiled_rhs.jl")
+include("solvers/lazy_rhs.jl")
 include("solvers/solver_utils.jl")
